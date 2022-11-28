c6eebc47f6c6a1c844897f456f711ad757d680b8
Richard Wang <riwa@umich.edu>
2022-11-28 17:03:50 -0500

    Homepage stuff

---
+++ js/nav.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.querySelector("#mobileSideNav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.querySelector("#mobileSideNav").style.width = "0";
+  }
