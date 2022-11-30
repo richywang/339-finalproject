f32357bbc69e43f0ee8933aea7102236127b03c3
richywang <richard.y.wang@gmail.com>
2022-11-30 02:16:15 -0500

    extras?

--- js/iceland.js
+++ js/iceland.js
@@ -5,23 +5,8 @@ $(document).ready(function() {
     if(!mediaQuery.matches) {
       /* Every time the window is scrolled ... */
       $(window).scroll( function(){
-      
-          /* Check the location of each desired element */
-          $('.travelsection').each( function(i){
-              
-              let top_of_object = $(this).position().top;
-              let bottom_of_window = $(window).scrollTop() + $(window).height();
-              
-              /* If the object is completely visible in the window, fade it it */
-              if( bottom_of_window+50 > top_of_object ){
-                  
-                  $(this).animate({'top': '0', 'opacity':'1'},1000);
-                      
-              }
 
-          }); 
-
-          $('.traveltextsection').each( function(i){
+          $('.flexrow').each( function(i){
               
             let top_of_object = $(this).position().top;
             let bottom_of_window = $(window).scrollTop() + $(window).height();
